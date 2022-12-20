827bf7d3b4cb2bc3db0e3ba38f240874344d8adc
Koby <koby@aztecprotocol.com>
2022-12-20 14:16:24 +0100

    adds proper config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-//const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
-
 const withTM = require("next-transpile-modules")([
   "@noir-lang/aztec_backend",
   "@noir-lang/barretenberg",
@@ -11,20 +9,10 @@ const withTM = require("next-transpile-modules")([
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
-  /*webpack: (config) => {
-    config.plugins.push( 
-        new NodePolyfillPlugin()
-    )
-
-  return config;
-  },*/
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
 
     config.resolve.mainFields = ['browser', 'module', 'main'];
-
-    // console.log(config.module.exports);
-    // config.target = "web",
 
     config.module.rules.push({
       test: /\.wasm$/,
